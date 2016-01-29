e201e311846fbdcb151ee24cf2bdb98eef0803a9
Colt Bauman <gameconstructor45@gmail.com>
2016-01-30 02:27:13 +0900

    Drag and Drop

---
+++ GGJ Project/Assets/Scripts/Process/DragDropSlot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragDropSlot : MonoBehaviour, IDropHandler
+{
+    private GameObject GetItem()
+    {
+        if(transform.childCount > 0)
+            return transform.GetChild(0).gameObject;
+        else
+            return null;
+    }
+
+    public void OnDrop( PointerEventData eventData )
+    {
+        Debug.Log("Drop");
+        if( GetItem() == null )
+        {
+            DragAndDrop.selectedObject.transform.SetParent( transform );
+            DragAndDrop.selectedObject.transform.localPosition = new Vector3(0, 0, -1);
+        }
+    }
+}
