ea1888d02a73ddd22b17e6a4de31a0e2243d07ae
Colt Bauman <gameconstructor45@gmail.com>
2016-01-30 14:51:32 +0900

    Zero Gravity Goat

---
+++ GGJ Project/Assets/Scripts/Object/ZeroGravityGoat.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ZeroGravityGoat : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public float initialForce = 100;
+    public float drag = .1f;
+    private Rigidbody2D thisRigidBody;
+    private Vector2 startTouchPos;
+    private float startTouchTime;
+
+
+    void Start()
+    {
+        thisRigidBody = GetComponent<Rigidbody2D>();
+        thisRigidBody.AddForce(new Vector2(-1, -1) * initialForce);
+    }
+
+    public void OnDrag( PointerEventData eventData )
+    {
+        //transform.position = Input.mousePosition;
+    }
+
+    public void OnBeginDrag( PointerEventData eventData )
+    {
+        startTouchPos = eventData.position;
+        startTouchTime = Time.time;
+        thisRigidBody.velocity = Vector2.zero;
+    }
+
+    public void OnEndDrag( PointerEventData eventData )
+    {
+        Vector2 deltaPos = eventData.position - startTouchPos;
+        float deltaTime = Time.time - startTouchTime;
+        Vector2 movement = deltaPos / deltaTime;
+        thisRigidBody.velocity = Vector2.zero;
+        thisRigidBody.AddForce(movement * drag);
+    }
+}
