d97be4d4b2bd9c53f68bb559e1f7cbc196a81ad5
penspanic <penspanic@gmail.com>
2016-01-30 17:56:30 +0900

    Third Merge

---
+++ GGJ Project/Assets/Scripts/Process/Title.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Title : MonoBehaviour
+{
+    public Text titleText;
+    void Awake()
+    {
+        StartCoroutine(MoveTitle());
+    }
+
+    public void OnStartButtonDown()
+    {
+
+    }
+
+    IEnumerator MoveTitle()
+    {
+        while(true)
+        {
+            titleText.transform.position = new Vector2(0, Mathf.Sin(5f));
+            yield return null;
+        }
+    }
+}
